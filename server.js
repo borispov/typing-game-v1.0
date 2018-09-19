3b17d59e446e25f5db77af5a70e1e610d6363cb0
Boris Povolotsky <boristofu@gmail.com>
2018-09-19 22:16:56 +0300

    fixed not needed server.js axios require

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
-const axios = require('axios')
 
 const scores = require('./routes/scorePost')
 
